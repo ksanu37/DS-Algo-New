915b6ef4dde4cb38f6e776537d693ac4e952f79d
Sanu <ksanu.37@gmail.com>
2021-07-14 23:04:09 +0530

    14/07

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import linkedlist.LinkedList;
+import linkedlist.PrintMiddleOfAGivenLinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int[] arr = {};
+        LinkedList list = LinkedList.convertArrayToLinkedList(arr);
+        LinkedList.printList(list);
+        System.out.println(PrintMiddleOfAGivenLinkedList.run(list));
+
     }
 }
